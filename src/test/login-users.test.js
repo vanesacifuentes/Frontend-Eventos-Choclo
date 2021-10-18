aa5a6afb46df61275f0cfc26768b66a657349e89
vanesacifuentes <vanesa.d8793@gmail.com>
2021-10-18 11:19:25 -0500

    Travis- pruebas

--- src/test/login-users.test.js
+++ src/test/login-users.test.js
@@ -3,16 +3,11 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import { createSerializer } from 'enzyme-to-json'
 
 Enzyme.configure({ adapter: new Adapter() })
-expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }))
+//expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }))
 
-import LoginUser from '../components/perfil_usuario/Login/login-user'
-//import Context from '../components/perfil_usuario/Context/Context'
-//import App from '../index'
-import React, { useContext } from 'react';
+import LoginUser from '../components/perfil_usuario/Login/login-user.js'
+import React from 'react';
 import { shallow } from 'enzyme'
-import { Router, Switch } from 'react-router'
-import Register_User from '../components/Perfil_usuario/Registro/register_user'
-import Context from '../components/Perfil_usuario/Context/Context'
 import App from '../App'
 import { UserProvider } from '../components/Perfil_usuario/Context/Context'
 
